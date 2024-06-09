2d9a49d21ca6da9cfd458c4741347b5cb720f135
joseptandrade <joseptandrade@hotmail.com>
2024-06-09 14:08:45 -0500

    Add files via upload

--- Pie_Pap_Tij_Spo_Lag.py
+++ Pie_Pap_Tij_Spo_Lag.py
@@ -12,16 +12,16 @@ numero = 1
 g1 = 0
 g2 = 0
 
-###Iniciamos nuestro bucle comparando la variable numero si es menor o igual
+###Iniciamos nuestro bucle comparando la variable número si es menor o igual
 ###Si se cumple ingresamos al bucle
 while numero <= i:
-    ###A la variable numero la vamos ingrementando en uno para luego realizar la comparacion al inicio del blucle
+    ###A la variable numero la vamos incrementando en uno para luego realizar la comparación al inicio del bucle
     numero += 1
-    ###Pedimos a los jugadores ingresar la opcion que van a elegir 
+    ###Pedimos a los jugadores ingresar la opción que van a elegir 
     j1 = int(input("Jugador 1 Ingrese una de las siguientes opciones: 1=Piedra, 2=Papel, 3=Tijeras, 4=Spock, 5=Lagarto: "))
     j2 = int(input("Jugador 2 Ingrese una de las siguientes opciones: 1=Piedra, 2=Papel, 3=Tijeras, 4=Spock, 5=Lagarto: "))
-    ###Iniciamos las condicionales para cada opcion de ingreso, al cumplirse se imprime mensaje con el numero del jugador que gane 
-    ##Comparamos los posibles ingresos para la opcion 1 que es Piedra
+    ###Iniciamos las condicionales para cada opción de ingreso, al cumplirse se imprime mensaje con el número del jugador que gane 
+    ##Comparamos los posibles ingresos para la opción 1 que es Piedra
     if j1 == 1 and j2 == 2:
             print("Jugador 2 Gana: Papel gana a Piedra")
             g2 +=1
@@ -34,7 +34,7 @@ while numero <= i:
     elif j1 == 1 and j2 == 5:
             print("Jugador 1 Gana: Piedra gana a Lagarto")
             g1 +=1
-    ##Comparamos los posibles ingresos para la opcion 2 que es Papel
+    ##Comparamos los posibles ingresos para la opción 2 que es Papel
     elif j1 == 2 and j2 == 1:
             print("Jugador 1 Gana: Papel gana a Piedra")    
             g1 +=1
@@ -47,7 +47,7 @@ while numero <= i:
     elif j1 == 2 and j2 == 5:
             print("Jugador 2 Gana: Lagarto gana a Papel")
             g2 +=1
-    ##Comparamos los posibles ingresos para la opcion 3 que es Tijeras
+    ##Comparamos los posibles ingresos para la opción 3 que es Tijeras
     elif j1 == 3 and j2 == 1:
         print("Jugador 2 Gana: Piedra gana a Tijeras") 
         g2 +=1
@@ -60,7 +60,7 @@ while numero <= i:
     elif j1 == 3 and j2 == 5:
         print("Jugador 1 Gana: Tijera gana a Lagarto")
         g1 +=1
-    ###Comparamos los posibles ingresos para la opcion 4 que es Spock
+    ###Comparamos los posibles ingresos para la opción 4 que es Spock
     elif j1 == 4 and j2 == 1:
         print("Jugador 1 Gana:  Spock gana a Piedra")
         g1 +=1
@@ -73,7 +73,7 @@ while numero <= i:
     elif j1 == 4 and j2 == 5:
         print("Jugador 2 Gana: Lagarto gana a Spock")
         g2 +=1
-    ###Comparamos l0s posibles ingresos para la opcion 5 que es lagarto
+    ###Comparamos los posibles ingresos para la opción 5 que es lagarto
     elif j1 == 5 and j2 == 1:
         print("Jugador 2 Gana: Piedra gana a Lagarto")
         g2 +=1
@@ -87,16 +87,16 @@ while numero <= i:
         print("Jugador 1 Gana: Lagarto gana a Spock")
         g1 +=1
     else:
-        ###En caso de no cumplise las condiciones se imprime lo siguiente
+        ###En caso de no cumplirse las condiciones se imprime lo siguiente
         print("Jugadores quedaron empate")
-### Comparamos si el contador g1 es mayo que g2 si se cumple la condicion se imprime lo siguiente
+### Comparamos si el contador g1 es mayo que g2 si se cumple la condición se imprime lo siguiente
 if g1 > g2:
      print("Jugador 1 has ganado", g1 ,"veces la partida")
      print("Eres el ganador de la partida, el juego a terminado gracias")
-### Comparamos si el contador g2 es mayo que g1 si se cumple la condicion se imprime lo siguiente
+### Comparamos si el contador g2 es mayo que g1 si se cumple la condición se imprime lo siguiente
 elif g2 > g1:
     print("Jugador 2 has ganado", g2 ,"veces la partida")
     print("Eres el ganador de la partida, el juego a terminado gracias")
 ###Caso contrario se imprime el empate 
 else:
-     print("Empataron la partida, el juego a terminado gracias") 
+     print("Empataron la partida, el juego a terminado gracias")
